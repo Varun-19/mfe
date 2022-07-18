5c2bc6a1f00a400acebcd329fd3ff2db95c6e14b
Varun-19 <varundravid290@gmail.com>
2022-07-18 20:58:00 +0530

    Test

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Home Page
+              Home Page of site
             </Typography>
             <Typography
               variant="h5"
